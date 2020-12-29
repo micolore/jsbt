0984639b531970b7a4660b56609169d4fd8a8ed5
kubrick <471304402@qq.com>
2020-12-29 12:42:19 +0800

    update gradle config

--- sbt-cache/src/main/java/com/kubrick/sbt/cache/controller/CacheController.java
+++ sbt-cache/src/main/java/com/kubrick/sbt/cache/controller/CacheController.java
@@ -2,6 +2,7 @@ package com.kubrick.sbt.cache.controller;
 
 import com.kubrick.sbt.cache.service.CacheService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class CacheController {
 	@Autowired
 	private CacheService cacheService;
 
-	@PostMapping("/cache")
+	@GetMapping("/cache")
 	public String test() {
 		cacheService.testRedisson();
 		return "Hello cache!";
