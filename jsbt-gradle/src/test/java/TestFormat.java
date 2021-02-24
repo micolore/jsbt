0d7f51e9cf19e6bd7606590a687f44958fb66017
kubrick <471304402@qq.com>
2021-02-24 23:21:57 +0800

    add starter boot

---
+++ jsbt-gradle/src/test/java/TestFormat.java
@@ -0,0 +1,13 @@
+/**
+ * @author k
+ * @version 1.0.0
+ * @ClassName TestFormat
+ * @description: TODO
+ * @date 2021/2/24 下午4:38
+ */
+public class TestFormat {
+    public static void main(String[] args) {
+        String limitSql = String.format("limit %d", 5);
+        System.out.println(limitSql);
+    }
+}
